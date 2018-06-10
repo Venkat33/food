91d721c66815be9f457961408778abfa58171842
Venkat Sabarinathan <sabarishzack@gmail.com>
2018-06-10 13:33:55 +0530

    media controller

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -1,11 +1,6 @@
 class MediasController < ApplicationController
+  def new
+  @media =Media.new
 
-    def new
-      @medias = Medias.new
-    end
-
-    private
-    def media_params
-      params.require(:media).permit(:username, :description, :user_id)
-    end
-    end
+  end
+end
